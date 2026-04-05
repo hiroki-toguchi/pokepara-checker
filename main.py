532503548937d5a7f238fd0c0ca20ebfad64ff71
hiroki-toguchi <hiroki.toguchi0315@gmail.com>
2026-04-05 22:27:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,61 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+# ブログURL
 URL = "https://sp.pokepara.jp/tokyo/m9/a10034/shop4995/gal/777801/blog/"
-WEBHOOK_URL = os.environ["WEBHOOK_URL"]
 
-def send_discord(title, link, img):
-    data = {
-        "embeds": [
-            {
-                "title": "新着ブログ！",
-                "description": title,
-                "url": link,
-                "image": {"url": img} if img else {},
-            }
-        ]
-    }
-    requests.post(WEBHOOK_URL, json=data)
+# 前回記事タイトルを取得
+last_file = "last.txt"
+try:
+    with open(last_file, "r") as f:
+        last_title = f.read().strip()
+except:
+    last_title = ""
 
-def get_latest():
-    res = requests.get(URL)
-    soup = BeautifulSoup(res.text, "html.parser")
+# 最新記事取得
+res = requests.get(URL)
+soup = BeautifulSoup(res.text, "html.parser")
+article = soup.select_one(".blog_list li")
 
-    article = soup.select_one(".blog_list li")
-    if not article:
-        return None, None, None
-
+if article:
     title = article.get_text(strip=True)
-
     link_tag = article.find("a")
     link = link_tag["href"] if link_tag else ""
     if link.startswith("/"):
         link = "https://sp.pokepara.jp" + link
-
     img_tag = article.find("img")
-    img = img_tag["src"] if img_tag else None
-    if img and img.startswith("/"):
+    img = img_tag["src"] if img_tag else ""
+    if img.startswith("/"):
         img = "https://sp.pokepara.jp" + img
 
-    return title, link, img
+    # 新着かチェック
+    if title != last_title:
+        # 新着あり
+        print(f"::set-output name=new::true")
+        print(f"::set-output name=title::{title}")
+        print(f"::set-output name=link::{link}")
+        print(f"::set-output name=img::{img}")
 
-def main():
-    title, link, img = get_latest()
-    if not title:
-        return
-
-    latest = title + link
-
-    try:
-        with open("last.txt", "r") as f:
-            old = f.read()
-    except:
-        old = ""
-
-    if latest != old:
-        send_discord(title, link, img)
-        with open("last.txt", "w") as f:
-            f.write(latest)
-
-if __name__ == "__main__":
-    main()
+        # 次回用に last.txt を更新
+        with open(last_file, "w") as f:
+            f.write(title)
+    else:
+        print("::set-output name=new::false")
+else:
+    print("::set-output name=new::false")
